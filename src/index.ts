e4409965a660c5df7d3ba554987a146952f1c081
FARUK2059 <omarfaruk34552@gmail.com>
2025-03-30 13:11:28 +0600

    all table data get success

--- src/index.ts
+++ src/index.ts
@@ -6,14 +6,24 @@ const prisma = new PrismaClient();
 
 
 const main = async () => {
-const result = await prisma.post.create({
-    data: {
-        title: "This is contant title",
-        content: "this is contant",
-        authorName: "MD OMAR FARUK",
-        author: "ami"
-    }
-})
+
+    // insert tavble data
+// const result = await prisma.post.create({
+//     data: {
+//         title: "This is contant title 2",
+//         content: "this is contant",
+//         authorName: "MD OMAR FARUK",
+//         author: "ami"
+//     }
+// });
+// console.log(result);
+
+// get all table data
+const getAllFromDB = await prisma.post.findMany();
+console.log(getAllFromDB);
+
+
+
 
 }
 
