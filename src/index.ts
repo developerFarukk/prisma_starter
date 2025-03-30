628b2be917cf7536ceed9669567bf763bc545ab7
FARUK2059 <omarfaruk34552@gmail.com>
2025-03-30 12:52:22 +0600

    data get

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,20 @@
 
 
-console.log("ph");
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+
+const main = async () => {
+const result = await prisma.post.create({
+    data: {
+        title: "This is contant title",
+        content: "this is contant",
+        authorName: "MD OMAR FARUK",
+        author: "ami"
+    }
+})
+
+}
+
+main();
