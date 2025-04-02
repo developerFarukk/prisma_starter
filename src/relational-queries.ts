f0efd19082da9a816aa37189036a30e298c6af1c
FARUK2059 <omarfaruk34552@gmail.com>
2025-04-02 21:46:19 +0600

    relat true

---
+++ src/relational-queries.ts
@@ -0,0 +1,42 @@
+
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const relationalQueries = async () => {
+
+    // fluent api
+    // const result = await prisma.user.findUnique({
+    //     where: {
+    //         id: 1
+    //     },
+    //     include: {
+    //         post: true
+    //     }
+    // })
+    // const result = await prisma.user.findUnique({
+    //     where: {
+    //         id: 1
+    //     },
+    // })
+    // // .post()
+    // .profile();
+
+    // console.log(result);
+    
+
+
+    // relational fillters
+    const publishedPostUsers = await prisma.user.findMany({
+        include: {
+            post: {
+                where: {
+                    published: true
+                }
+            }
+        }
+    })
+    console.dir(publishedPostUsers, { depth: Infinity })
+};
+
+relationalQueries();
