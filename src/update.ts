b90234172126bfabf74696493e8040824a96a082
FARUK2059 <omarfaruk34552@gmail.com>
2025-04-02 13:33:47 +0600

    upsert

--- src/update.ts
+++ src/update.ts
@@ -25,32 +25,36 @@ const updates = async () => {
 
 
     // update many
-    const updateMany = await prisma.post.updateMany({
-        where: {
-            title: "title 2"
-        },
-        data: {
-            published: true
-        }
-    })
-
-    console.log(updateMany)
-
-
-    // const upsertData = await prisma.post.upsert({
+    // const updateMany = await prisma.post.updateMany({
     //     where: {
-    //         id: 10
+    //         title: "title 2"
     //     },
-    //     update: {
-    //         authorName: "Fahim"
-    //     },
-    //     create: {
-    //         title: "Title 2",
-    //         content: "content 2"
+    //     data: {
+    //         published: true
     //     }
-    // });
+    // })
 
-    // console.log(upsertData)
+    // console.log(updateMany)
+
+
+    // update or create data
+    const upsertData = await prisma.post.upsert({
+        where: {
+            id: 16
+        },
+        update: {
+            authorName: "Faruk",
+            title: "Title 16",
+            content: "content 16"
+        },
+        create: {
+            title: "Title 15",
+            content: "content 2",
+            author: "Faruk"
+        }
+    });
+
+    console.log(upsertData)
 };
 
 updates();
