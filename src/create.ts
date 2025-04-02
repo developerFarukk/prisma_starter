5bbcf40132b34d2fbea16cc7157aafd6200c3bbd
FARUK2059 <omarfaruk34552@gmail.com>
2025-04-02 13:01:23 +0600

    create many

--- src/create.ts
+++ src/create.ts
@@ -1,30 +1,78 @@
 
 
-
-
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient,  } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-
 const main = async () => {
-
-    // insert tavble data
-    // const result = await prisma.post.create({
+    // const createUser = await prisma.user.create({
     //     data: {
-    //         title: "This is contant title 2",
-    //         content: "this is contant",
-    //         authorName: "MD OMAR FARUK",
-    //         author: "ami"
+    //         username: "user2",
+    //         email: "user2@ph.com",
+    //         role: UserRole.user
     //     }
     // });
-    // console.log(result);
 
-    // get all table data
-    const getAllFromDB = await prisma.post.findMany();
-    console.log(getAllFromDB);
+    // const createProfile = await prisma.profile.create({
+    //     data: {
+    //         bio: "this is bio...",
+    //         userId: 1
+    //     }
+    // })
 
+    // const createCategory = await prisma.category.create({
+    //     data: {
+    //         name: "software engineering"
+    //     }
+    // })
 
+    const createPost = await prisma.post.createMany({
+        data: [
+            {
+                title: "title 1",
+                content: "contant 1",
+                authorName: "author name 1",
+                author: "author 1"
+            },
+            {
+                title: "title 2",
+                content: "contant 2",
+                authorName: "author name 2",
+                author: "author 2"
+            },
+            {
+                title: "title 3",
+                content: "contant 3",
+                authorName: "author name 3",
+                author: "author 3"
+            }
+        ]
+    })
+
+    // const createPost = await prisma.post.create({
+    //     data: {
+    //         title: "this is title 5",
+    //         content: "this is content of the post. 5",
+    //         authorId: 3,
+    //         postCategory: {
+    //             create: [
+    //                 {
+    //                     categoryId: 1
+    //                 },
+    //                 {
+    //                     categoryId: 3
+    //                 },
+    //                 {
+    //                     categoryId: 4
+    //                 }
+    //             ]
+    //         }
+    //     },
+    //     include: {
+    //         postCategory: true
+    //     }
+    // })
+    console.log(createPost)
 }
 
 main();
