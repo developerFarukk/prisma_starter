828f658f8b12eb47af6a6d5090145c863488d4aa
FARUK2059 <omarfaruk34552@gmail.com>
2025-04-05 13:22:08 +0600

    up basic

--- src/raw-query.ts
+++ src/raw-query.ts
@@ -4,11 +4,11 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 const rawQuery = async () => {
-    // const posts = await prisma.$queryRaw`SELECT * FROM "posts"`
-    // console.log(posts)
+    const posts = await prisma.$queryRaw`SELECT * FROM "posts"`
+    console.log(posts)
 
     // delete users table data
-    await prisma.$queryRaw`TRUNCATE TABLE "users" CASCADE`
+    // await prisma.$queryRaw`TRUNCATE TABLE "users" CASCADE`
 };
 
 rawQuery();
